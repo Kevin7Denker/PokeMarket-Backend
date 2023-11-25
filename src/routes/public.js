c41a90822a5174c3a859a15d48100565042e11d3
Kevin Denker <kevindenker78@gmail.com>
2023-11-25 18:13:52 -0300

    .

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const route = express.Router();
 
+
+
 route.get("/", (req, res) => {
   res.status(200).json({ msg: "Welcome to Denker's Api" });
 });
+
+
+
 
 module.exports = route;
